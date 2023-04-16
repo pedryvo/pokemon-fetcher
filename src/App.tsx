40ce940f6c6511ef31377136847b9beec1dc4b23
Pedro Ivo <pedryvo@gmail.com>
2023-04-16 20:08:20 -0300

    vercel error

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const navItems = [
   /* { title: 'Home', link: '/' },
   { title: 'About', link: '/about' },
   { title: 'Contact', link: '/contact' } */
-  {}
+  { title: '', link: '' }
 ];
 
 const dropdownItems = [
